609e15c08ed5dc812f1cb37be4e76afc1b0ffbd5
shorab.rustam <shorab.rustam@mpokket.com>
2023-05-28 15:50:41 +0530

    return as sturct and function and structure

--- EssentialCPP/structureandfunction.cpp
+++ EssentialCPP/structureandfunction.cpp
@@ -3,6 +3,14 @@ using namespace std;
 struct Rectangle{
     int width,height;
 };
+void initilize(struct Rectangle * r,int height,int width){
+    r->height = height;
+    r->width = width;
+    return;
+}
+struct Rectangle * get(struct Rectangle * r){
+    return r;
+}
 int area(struct Rectangle r){
     return r.width*r.height;
 }
@@ -11,8 +19,11 @@ void changeLength(struct Rectangle *r,int height){
     return;
 }
 int main(int argc,char **argv){
-    struct Rectangle rect={10,20};
+    struct Rectangle rect;
+    initilize(&rect,5,10);
     cout << "Area : "<<area(rect)<<endl;
     changeLength(&rect,100);
+    struct Rectangle *r = get(&rect);
+    cout << "height : "<<r->height<<endl;
     cout << "Area After change height: "<<area(rect)<<endl;
 }
