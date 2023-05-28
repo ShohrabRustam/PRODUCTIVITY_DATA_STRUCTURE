2956c57853c5e57f396b88b774460dd486cae4b6
shorab.rustam <shorab.rustam@mpokket.com>
2023-05-28 14:52:45 +0530

    call by address

---
+++ EssentialCPP/callbyaddress.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int add(int *a, int *b){
+    return *a + *b;
+}
+int main(int argc,char  **argv){
+    int n = 5;
+    int m = 6;
+    int sum = add(&n,&m);//actual parameters
+    cout <<n << " + "<<m<<" =  "<< sum  << endl;
+}
