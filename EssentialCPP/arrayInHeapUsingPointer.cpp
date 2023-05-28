f0255d1dc799906ee76209ab6ebd812caef2a6b6
shorab.rustam <shorab.rustam@mpokket.com>
2023-05-28 11:38:06 +0530

    make array in heap using pointer

---
+++ EssentialCPP/arrayInHeapUsingPointer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(int argc, char** argv){
+    int *ptr;
+    ptr = new int[10];
+    for (int i = 0; i < 10; i++)
+    {
+        ptr[i] = i;
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        cout <<ptr[i] << endl;
+    }
+    delete [] ptr;
+    return 0;
+    
+}
