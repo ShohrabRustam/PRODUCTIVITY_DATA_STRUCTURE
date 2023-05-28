82bc45a01b6d3582b9937f8e31c412cfea8d524b
shorab.rustam <shorab.rustam@mpokket.com>
2023-05-28 14:05:14 +0530

    pointer to structure

--- EssentialCPP/pointertostructure.cpp
+++ EssentialCPP/pointertostructure.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <stdlib.h>
 using namespace std;
 struct Rectangle{
     int width,height;
@@ -7,5 +8,23 @@ int main(int argc, char** argv){
     struct Rectangle rect = {10,20};
     struct Rectangle *ptr= &rect;
     cout<<"width: "<<ptr->width<<" height: "<<ptr->height<<endl;
+    delete [] ptr;
+
+    struct Rectangle *ptrarray;
+    ptrarray =  new struct Rectangle[10] ;
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout <<"HIH"<<endl;
+        ptrarray[i].width = i;
+        ptrarray[i].height = i+10;
+    }
+    for (int i = 0; i < 10; i++)
+    {
+        cout<<"count : "<<i<<" width: "<<ptrarray[i].width<<" height: "<<ptrarray[i].height<<endl;
+    }
+
+    delete [] ptrarray;
+    
     return 0;
 }
