412689e09a08b46dc93aa54ec2ce07719c9ce1da
shorab.rustam <shorab.rustam@mpokket.com>
2023-05-28 15:42:03 +0530

    passing structure in function

--- EssentialCPP/structureasparameter.cpp
+++ EssentialCPP/structureasparameter.cpp
@@ -6,7 +6,14 @@ struct Rectangle{
 int area(struct Rectangle r){
     return r.width*r.height;
 }
+void changeLength(struct Rectangle *r,int height){
+    r->height = height;
+    return;
+}
 int main(int argc,char **argv){
     struct Rectangle rect={10,20};
-    cout << "Area : "<<area(rect);
+    cout << "Area : "<<area(rect)<<endl;
+    changeLength(&rect,100);
+    cout << "Area After change height: "<<area(rect)<<endl;
+
 }
--- EssentialCPP/tempCodeRunnerFile.cpp
+++ EssentialCPP/tempCodeRunnerFile.cpp
@@ -1,3 +1,1 @@
-or(int i = 0; i < n;i++){
-        Arr[i] = i+5;
-    }
+height
