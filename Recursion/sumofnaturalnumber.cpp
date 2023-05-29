c0537e867b36f765a31e3a49470d06c3bfedf0e7
shorab.rustam <shorab.rustam@mpokket.com>
2023-05-29 18:59:25 +0530

    sum of of n natural number using recursion

---
+++ Recursion/sumofnaturalnumber.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int sumofnaturalnumber(int n){
+    if(n==0) return 0;
+    return n + sumofnaturalnumber(n-1);
+}
+int main(int argc, const char** argv) {
+    int n = 100;
+    cout <<"sum of "<< n <<" natural number is : "<<sumofnaturalnumber(n)<< endl;
+    return 0;
+}
