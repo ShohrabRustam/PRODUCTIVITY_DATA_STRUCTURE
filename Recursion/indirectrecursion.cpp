d58c4cb12528bc47c519b3d330333c74a867e34d
shorab.rustam <shorab.rustam@mpokket.com>
2023-05-29 18:40:31 +0530

    check the odd or even with negative case using recursion with indirect recursion

--- Recursion/indirectrecursion.cpp
+++ Recursion/indirectrecursion.cpp
@@ -6,7 +6,11 @@ string isEven(int n){
     if (n==0){
         return "ODD";
     }
-    result = isOdd(n-1);
+    if (n<0){
+        result = isOdd(n+1);
+    }else{
+        result = isOdd(n-1);
+    }
     return result;
 }
 
@@ -14,13 +18,17 @@ string isOdd(int n){
     if (n==0){
         return "EVEN";
     }
-    result = isEven(n-1);
+    if (n<0){
+        result = isEven(n+1);
+    }else{
+        result = isEven(n-1);
+    }
     return result;
 }
 
 
 int main(){
-    int n=50;
+    int n=-51;
     cout <<n<<" is : "<< isOdd(n) << endl;
     return 0;
 }
