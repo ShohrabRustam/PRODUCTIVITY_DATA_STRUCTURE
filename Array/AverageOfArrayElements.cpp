25c052dc5f7c1efd1e31b60113403c1dd019949a
shorab.rustam <shorab.rustam@mpokket.com>
2023-06-12 23:29:21 +0530

    average of an array elements

---
+++ Array/AverageOfArrayElements.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+float Avg(int A[],int size){
+    int Sum =0;
+    for(int i=0;i<size;i++){
+            Sum+= A[i];
+    }
+    return (float)Sum/size;
+   
+}
+int main(int argc, char** argv){
+    int size = 10;
+    int *p;
+     p  = new int[size];
+
+    for(int i = 0; i < size; i++){
+        p[i] = 2*(-i);
+    }
+    cout << Avg(p,10) << endl;
+    return 0;
+}
