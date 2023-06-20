f86cd106a16e4244d3ce735e23768af70f10c056
shorab.rustam <shorab.rustam@mpokket.com>
2023-06-20 10:18:03 +0530

    left shift operator

--- BitwiseOperator/rightShift.cpp
+++ BitwiseOperator/rightShift.cpp
@@ -1,10 +1,10 @@
 #include <iostream>
 using namespace std;
 /*
-    a<<3;=> a* 2(power)3 in most of the cases for small numbers
+    a<<3;=> a/ 2(power)3 in most of the cases for small numbers
 */
 int main(int argc, char** argv){
     int a = 3;
-    int c= a<<2; //3 * (2*2)
+    int c= a>>2; // 3 /(2*2) = 0
     cout << "a = " << a <<", a << 2 => " << c <<endl;
 }
