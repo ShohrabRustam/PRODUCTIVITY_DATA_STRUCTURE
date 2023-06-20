e30ebf5a3b778ea584c8bb1ad992f8091e4ac09b
shorab.rustam <shorab.rustam@mpokket.com>
2023-06-20 09:37:34 +0530

    or opearator

--- Pattern/andOpearator.cpp
+++ Pattern/andOpearator.cpp
@@ -3,6 +3,6 @@ using namespace std;
 int main(int argc, char ** arg){
     int a = 3;
     int b = 2;
-    int c= a&b; // 101 & 010 = 010 =>2
+    int c= a&b; // 11 & 10 = 010 =>2
     cout << "a = " << a << ", b = " << b << ", a & b => " << c <<endl;
 }
