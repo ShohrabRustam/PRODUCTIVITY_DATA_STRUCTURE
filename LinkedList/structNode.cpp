ac9a230844e638a4869784e035002cc9399f87e0
shorab.rustam <shorab.rustam@mpokket.com>
2023-11-06 22:41:56 +0530

    node stucture and access

--- LinkedList/structNode.cpp
+++ LinkedList/structNode.cpp
@@ -1,5 +1,14 @@
+#include <iostream>
+using namespace std;
 struct Node
 {
     int data;
     struct Node *next;
 };//such a structure known as self referential structure
+
+int main (){
+    struct Node *p;
+     p = new Node;
+     p->data = 5;
+     p->next = NULL;
+}
