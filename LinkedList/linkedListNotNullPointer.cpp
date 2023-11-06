e510f719be71874e9685929c470eb2f2dd9a5d81
shorab.rustam <shorab.rustam@mpokket.com>
2023-11-06 22:53:09 +0530

    check pointer not null in linked list

---
+++ LinkedList/linkedListNotNullPointer.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+};//such a structure known as self referential structure
+
+int main (){
+    struct Node *p;
+    // condition to check the pointer is not pointing anywhere
+    if(p!=0) 
+    // true;
+    if(p!=NULL)
+    //true;
+    if(p)
+    // true;
+}
