2c458aaffd467f52acd4b3257e5d2456a857e0ed
shorab.rustam <shorab.rustam@mpokket.com>
2023-11-06 22:44:48 +0530

    null pointer

---
+++ LinkedList/linkedListNullPointer.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+struct Node
+{
+    int data;
+    struct Node *next;
+};//such a structure known as self referential structure
+
+int main (){
+    struct Node *p = NULL;
+}
