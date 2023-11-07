94fb34219afc5e5426dd1d319be0e247203b1787
shorab.rustam <shorab.rustam@mpokket.com>
2023-11-07 21:31:29 +0530

    create Node functionality

--- LinkedList/LinkedList.cpp
+++ LinkedList/LinkedList.cpp
@@ -5,3 +5,15 @@ struct Node{
     int data;
     struct Node *next;
 };
+
+struct Node * createNode(data){
+    struct Node *newNode = new Node;
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+int main(){
+
+
+}
