31b90c32c19c8ca6148113bf9937a4cd6cc418d6
Shohrab Rustam <mohdrustam001@gmail.com>
2025-04-27 16:18:55 +0530

    static variable

---
+++ Recursion/Recursion Revise/staticvar.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+ 
+int fun(int n)
+{
+    static int i = 0;
+    cout<<i++<<endl;
+    
+    if(n==0)
+        return 0;
+    
+    fun(n-1) + 1;
+    
+    cout<<"return call "<<n<<" "<<endl;
+    
+    return 0;
+}
+int main()
+{ 
+    // static variable
+    // static variable is used to store the value of a variable
+    // even after the function call is completed
+    // static variable is initialized only once
+    // static variable is used to store the value of a variable
+    int a = 5;
+    fun(a);
+    return 0;
+}
