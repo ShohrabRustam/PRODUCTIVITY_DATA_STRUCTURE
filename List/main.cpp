802416eee11c9852b9cadbe3013586c4eb639944
Shohrab Rustam <mohdrustam001@gmail.com>
2025-05-03 12:42:53 +0530

    Node

--- List/main.cpp
+++ List/main.cpp
@@ -4,8 +4,11 @@
 using namespace std;
 
 int main() {
-    Node* head = new Node{1, nullptr};
-    cout << "Data: " << head->data << endl;
+    // Node* head = new Node{1, nullptr};
+    Node * head = new Node;
+    head->data = 1;
+    head->next = nullptr;
+    cout << "Node created with data: " << head->data << endl;
     delete head;
     return 0;
 }
