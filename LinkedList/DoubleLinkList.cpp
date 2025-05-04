8ff731080d8b045d17970419238883378aaacaaa
Shohrab Rustam <mohdrustam001@gmail.com>
2025-05-04 23:28:10 +0530

    doubly list

---
+++ LinkedList/DoubleLinkList.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+using namespace std;
+
+struct Node{
+    int data;
+    struct Node *next;
+    struct Node *prev;
+};
+
+ Node * createNode(int data){
+    Node *newNode = new Node;
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+void createLinkList(struct Node ** head,int data){
+    Node *newNode = createNode(data);
+    
+    if(!*head){
+        *head = newNode;
+    }else{
+       Node *p;
+       p = *head;
+        while(p->next){
+            p=p->next;
+        }
+        p->next = newNode;
+        newNode->prev = p;
+    }
+}
+
+void printLinkedList(Node* root) {
+    Node *p = root;
+   while (p != NULL) {
+      cout << p->data << " -> ";
+      p = p->next;
+   }
+   cout << "NULL" << endl;
+   delete[] p;
+}
+
+int main(){
+    Node *head = NULL;
+    createLinkList(&head,10);
+    createLinkList(&head,20);
+    createLinkList(&head,30);
+    createLinkList(&head,40);
+    
+    printLinkedList(head);
+
+}
