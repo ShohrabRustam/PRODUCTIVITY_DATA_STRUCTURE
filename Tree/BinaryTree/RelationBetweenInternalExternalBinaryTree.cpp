89524aef39571bcedc1f84756d563e01ea0b2e1f
Shohrab Rustam <mohdrustam001@gmail.com>
2025-05-12 07:46:25 +0530

    The relation between the number of internal and external nodes in a binary tree is given by the following formula:

---
+++ Tree/BinaryTree/RelationBetweenInternalExternalBinaryTree.cpp
@@ -0,0 +1,7 @@
+The relation between the number of internal and external nodes in a binary tree is given by the following formula:
+
+The number of internal nodes (I) is equal to the number of external nodes (E) minus one:
+
+                        I = E - 1
+
+This means that in a binary tree, the number of internal nodes is always one less than the number of external nodes. This relationship holds true for all binary trees, regardless of their structure or size.
