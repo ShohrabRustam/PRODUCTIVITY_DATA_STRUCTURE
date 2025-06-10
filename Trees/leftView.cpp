26f62d2e6c53b3f3f17f4e93a0469a4b5bf07cb0
Shohrab Rustam <mohdrustam001@gmail.com>
2025-06-10 19:08:53 +0530

    right view

--- Trees/leftView.cpp
+++ Trees/leftView.cpp
@@ -34,30 +34,6 @@ vector<int> leftView(TreeNode* root) {
     return result;
 }
 
-vector<int> rightView(TreeNode* root) {
-    vector<int> result;
-    if (!root) return result;
-
-    queue<TreeNode*> q;
-    q.push(root);
-
-    while (!q.empty()) {
-        int size = q.size();
-        
-        for (int i = 0; i < size; ++i) {
-            TreeNode* node = q.front();
-            q.pop();
-
-            if (i == size - 1) {
-                result.push_back(node->val);
-            }
-
-            if (node->left) q.push(node->left);
-            if (node->right) q.push(node->right);
-        }
-    }
-    return result;
-
 bool isCousin(TreeNode* root, int x, int y) {
     if (!root) return false;
 
