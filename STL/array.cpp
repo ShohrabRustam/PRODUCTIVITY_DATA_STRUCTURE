2e350d644d123dfac090d16dfb22e595d566af7b
Shohrab Rustam <mohdrustam001@gmail.com>
2025-09-09 22:20:36 +0530

    array stl

---
+++ STL/array.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <array>
+using namespace std;
+ 
+int main()
+{
+    int basic[3] = {10, 20, 30};
+    array<int, 3> a = {10, 20, 30};
+    int size = a.size();
+    cout << "Size of array is " << size << endl;
+    cout << "Elements of array are: ";
+    for (int i = 0; i < size; i++) {
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    cout << "Elements of array using at() are: ";
+    for (int i = 0; i < size; i++) {
+        cout << a.at(i) << " ";
+    }
+    cout << endl; 
+    return 0;
+}
