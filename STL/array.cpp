9fd726c222082692cf8deb2e522da1b055dace06
Shohrab Rustam <mohdrustam001@gmail.com>
2025-09-09 22:21:50 +0530

    array

--- STL/array.cpp
+++ STL/array.cpp
@@ -8,6 +8,12 @@ int main()
     array<int, 3> a = {10, 20, 30};
     int size = a.size();
     cout << "Size of array is " << size << endl;
+
+    for(int i = 0; i < size; i++) {
+        cout << "Enter at index " << endl;
+        cin >> a[i];
+    }   
+    
     cout << "Elements of array are: ";
     for (int i = 0; i < size; i++) {
         cout << a[i] << " ";
